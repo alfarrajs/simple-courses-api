89a3d19381c7033d1887e9201a7d555c2dc4335b
Izzedeen Alfarra <ezazozqq123321@gmail.comm>
2023-09-11 19:00:59 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,97 @@
+const express = require("express");
+const app = express();
+const { body, validationResult } = require("express-validator");
+// CRUD (Create, Read, Update, Delete) - RESTful API (Representational State Transfer)
+// HTTP methodes : GET, POST, PUT, DELETE
+const courses = [
+  { id: 1, name: "react", price: 1000 },
+  { id: 2, name: "html", price: 2000 },
+  { id: 3, name: "css", price: 3000 },
+];
+
+app.get("/api/courses", (req, res) => {
+  res.send(courses);
+});
+
+app.get("/api/courses/:id", (req, res) => {
+  let wantedCourse = courses.find(
+    (courses) => courses.id === parseInt(req.params.id)
+  );
+  if (!wantedCourse)
+    res.status(404).send("The course with the given ID was not found.");
+  res.send(wantedCourse);
+  // some,every return boolean
+  //find return object , if not found return undefined
+  //filter return array , if not found return empty array
+
+  // different between res.send and res.write
+  // res.send() has by default res.end() in it , so you can't use res.write() after res.send()
+});
+
+// Parse JSON request bodies
+app.use(express.json());
+
+app.post(
+  "/api/courses",
+  [
+    body("name")
+      .notEmpty()
+      .withMessage("title/name of book is required")
+      .isString()
+      .withMessage("title/name of book must be a string"),
+    body("price")
+      .notEmpty()
+      .withMessage("price is required")
+      .isNumeric()
+      .withMessage("price must be a number")
+      .isInt({ min: 0 })
+      .withMessage("price must be a positive number"),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    // ?I use validationResult to check if there is an error in the request body
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json(errors.formatWith(({ msg }) => msg).mapped());
+    }
+
+    // ?I use return to stop the function from running
+    // ?I use formatWith to get the error message only
+
+    courses.push({ id: courses.length + 1, ...req.body });
+    res.status(201).json(courses);
+  }
+);
+// end the post request
+
+app.put("/api/courses/:id", (req, res) => {
+  let wantedCourse = courses.find(
+    (courses) => courses.id === parseInt(req.params.id)
+  );
+  if (!wantedCourse)
+    res.status(404).send("The course with the given ID was not found.");
+  wantedCourse.name = req.body.name;
+  wantedCourse.price = req.body.price;
+  res.send(wantedCourse);
+});
+// end the put request
+
+app.delete("/api/courses/:id", (req, res) => {
+  let wantedCourse = courses.find(
+    (courses) => courses.id === parseInt(req.params.id)
+  );
+  if (!wantedCourse)
+    res.status(404).send("The course with the given ID was not found.");
+  let index = courses.indexOf(wantedCourse);
+  courses.splice(index, 1);
+  res.send(wantedCourse);
+});
+// end the delete request
+
+app.listen(1900, () => {
+  console.log("Server running on port 1900");
+});
+
+// experss has by default a body parser , so you don't need to install it
+// body parser is a middleware that parse the body of the request
+// when you send a request to the server , the request has a body , the body is the data that you send to the server , which is in object form , but the server can't read it , so the body parser parse it to be readable by the server
